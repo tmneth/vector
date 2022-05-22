db781b4bce5bbfd17a8e617e7d59b2c357037a31
tmneth <80415416+tmneth@users.noreply.github.com>
2022-05-22 15:33:11 +0300

    Update custom vector class functionality

--- src/vector.hpp
+++ src/vector.hpp
@@ -1,61 +1,208 @@
 #pragma once
-#include <memory>
 
-using namespace std;
+#include <memory>
+#include <exception>
+#include <limits>
+#include <iostream>
 
 template<class T>
 
-class Vector {
+class myVector {
 
 public:
     typedef T *iterator;
     typedef const T *const_iterator;
+    typedef std::reverse_iterator<iterator> reverse_iterator;
+    typedef std::reverse_iterator<const_iterator> const_reverse_iterator;
     typedef const size_t size_type;
     typedef T value_type;
+    typedef value_type &reference;
+    typedef const value_type &const_reference;
+    typedef std::allocator<T> allocator_type;
 
-    // Constructor. Constructs an empty Vector container
-    Vector() { create(); }
+    // Member functions (constructor, destructor, operator=, assign, get_allocator)
 
-    // Use rule of three. Copy Constructor, Copy Assignment Operator, Destructor
+    // Constructor. Constructs an empty myVector container
+    myVector() { create(); }
 
-    // Fill empty Vector container with the 'size' number of 'val' copies
-    explicit Vector(size_type container_size, const T &val = T{}) { create (container_size, val); }
+    // Destructor. Deallocates memory and do other cleanup when the object is destroyed
+    ~myVector() { uncreate(); }
 
-    //  Copy Constructor
-    Vector(const Vector &vector) {
-        create(vector.begin(), vector.end());
+    // Fills empty Vector container with the 'size' number of 'val' copies
+    explicit myVector(size_type container_size, const T &val = T{}) { create(container_size, val); }
+
+    // Initializes Vector from an array
+    myVector(const std::initializer_list<T> list) {
+        create(list.begin(), list.end());
     }
 
-    // Destructor. Deallocates memory and do other cleanup for a class object and its class members when the object is destroyed.
-    ~Vector() { uncreate(); }
+    // Copies Constructor
+    myVector(const myVector &myVector) {
+        create(myVector.begin(), myVector.end());
+    }
 
 
-    T &operator[](size_type i) { return data[i]; }
-    const T &operator[](size_type i) const { return data[i]; }
+    // Assignment operators
+    void assign(size_type container_size, const T &val = T{}) {
+        uncreate();
+        create(container_size, val);
+    }
 
-    // Iterator pointing at the beginning of the Vector
+    void assign(std::initializer_list<value_type> list) {
+        uncreate();
+        create(list.begin(), list.end());
+    }
+
+
+    // Returns the allocator associated with the container.
+    allocator_type get_allocator() const { return alloc; };
+
+
+
+    // Element access  functions (at, operator[], front, back)
+
+
+
+    // Returns a reference to the element at specified location position. Bounds checking is performed.
+    reference at(size_type index) {
+        try {
+            if (index >= size() || index < 0)
+                throw std::domain_error("specified index is out of range");
+            else
+                return data[index];
+        }
+        catch (std::domain_error &e) {
+            std::cerr << "Terminate called after: " << e.what() << std::endl;
+        };
+    }
+
+    // Returns a reference to the element at specified location position. Bounds checking is performed.
+    const_reference at(size_type index) const {
+        try {
+            if (index >= size() || index < 0)
+                throw std::domain_error("specified index is out of range");
+            else
+                return data[index];
+        }
+        catch (std::domain_error &e) {
+            std::cerr << "Terminate called after: " << e.what() << std::endl;
+        };
+    }
+
+    // Returns a reference to the element at specified location pos. Bounds checking is performed.
+    T &operator[](size_type index) {
+        try {
+            if (index >= size() || index < 0)
+                throw std::domain_error("specified index is out of range");
+            else
+                return data[index];
+        }
+        catch (std::domain_error &e) {
+            std::cerr << "Terminate called after: " << e.what() << std::endl;
+        };
+    }
+
+    // Returns a reference to the element at specified location pos. Bounds checking is performed.
+    const T &operator[](size_type index) const {
+        try {
+            if (index >= size() || index < 0)
+                throw std::domain_error("specified index is out of range");
+            else
+                return data[index];
+        }
+        catch (std::domain_error &e) {
+            std::cerr << "Terminate called after: " << e.what() << std::endl;
+        };
+    }
+
+    // Returns a reference to the first element in the container.
+    reference front() { return *data; }
+
+    const_reference front() const { return *data; }
+
+    // Returns a reference to the last element in the container.
+    reference back() { return *(avail - 1); }
+
+    const_reference back() const { return *(avail - 1); }
+
+
+
+    // Iterators (begin, end, rbegin, rend)
+
+
+
+    // Returns an iterator to the first element of the vector.
     iterator begin() { return data; };
     const_iterator begin() const { return data; }
 
-    // Iterator pointing at the end of the Vector
+    // Returns an iterator to the element following the last element of the vector.
     iterator end() { return avail; }
     const_iterator end() const { return avail; }
 
-    // Space allocated for the Vector class
-    size_type capacity() const { return limit - data; }
+    // Returns a reverse iterator to the first element of the reversed vector.
+    // It corresponds to the last element of the non-reversed vector.
+    reverse_iterator rbegin() { return reverse_iterator(avail); }
+    const_reverse_iterator rbegin() const { return reverse_iterator(avail); }
 
-    // Alternative to std::vector size() method
+    // Returns a reverse iterator to the element following the last element of the reversed vector.
+    // It corresponds to the element preceding the first element of the non-reversed vector.
+    reverse_iterator rend() { return reverse_iterator(data); }
+    const_reverse_iterator rend() const { return reverse_iterator(data); }
+
+
+
+    //  Capacity (empty, size, max_size, reserve, capacity, shrink_to_fit)
+
+
+
+    // Checks if the container has no elements, i.e. whether begin() == end().
+    bool empty() { return !size(); }
+
+    // Returns the number of elements in the container, i.e. std::distance(begin(), end()).
     size_type size() const { return avail - data; }
 
-    // Alternative to std::vector push_back() method
-    void push(const T &val) {
+    // Returns the maximum number of elements the container is able to hold
+    // https://stackoverflow.com/questions/7949486/how-is-max-size-calculated-in-the-function-max-size-in-stdlist
+    size_type max_size() const noexcept {
+        return std::numeric_limits<size_type>::max();
+    }
+
+    // Increases the capacity of the vector.
+    void reserve(size_type new_cap) {
+        try {
+            if (new_cap > max_size())
+                throw std::length_error("new capacity exceeds maximum vector size");
+            else if (capacity() < new_cap) grow(new_cap);
+        }
+        catch (std::length_error &e) {
+            std::cerr << "Terminate called after: " << e.what() << std::endl;
+        };
+    }
+
+    // Returns the number of elements that the container has currently allocated space for.
+    size_type capacity() const { return limit - data; }
+
+    // Requests the removal of unused capacity.
+    void shrink_to_fit() { if (limit > avail) limit = avail; }
+
+
+
+    // Modifiers (clear, insert, erase, push_back, pop_back, resize, swap)
+
+
+
+    // Erases all elements from the container. After this call, size() returns zero.
+    void clear() { uncreate(); }
+
+    // Appends the given element value to the end of the container.
+    void push_back(const T &val) {
         if (avail == limit)
             grow();
         unchecked_append(val);
     }
 
-    // Alternative to std::vector pop_back() method
-    void pop() {
+    // Removes the last element of the container.
+    void pop_back() {
         iterator new_avail = avail;
         alloc.destroy(--new_avail);
         avail = new_avail;
@@ -65,7 +212,7 @@ private:
     iterator data;
     iterator limit;
     iterator avail;
-    allocator<T> alloc;
+    std::allocator<T> alloc;
 
     void create() {
         data = avail = limit = nullptr;
@@ -80,7 +227,7 @@ private:
 
     void create(const_iterator i, const_iterator j) {
         data = alloc.allocate(j - i);
-        limit = avail = uninitialized_copy(i, j, data);
+        limit = avail = std::uninitialized_copy(i, j, data);
     }
 
 
@@ -95,10 +242,10 @@ private:
         data = limit = avail = nullptr;
     }
 
-    void grow() {
-        size_type new_size = max(2 * (limit - data), ptrdiff_t(1));
+    void grow(size_type n = 1) {
+        size_type new_size = std::max(2 * (limit - data), ptrdiff_t(n));
         iterator new_data = alloc.allocate(new_size);
-        iterator new_avail = uninitialized_copy(data, avail, new_data);
+        iterator new_avail = std::uninitialized_copy(data, avail, new_data);
 
         uncreate();
 
@@ -107,6 +254,7 @@ private:
         limit = new_data + new_size;
     }
 
+    // Insert new element at the end
     void unchecked_append(const T &value) {
         alloc.construct(avail++, value);
     }
